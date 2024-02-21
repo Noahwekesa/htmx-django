119751ea30959910d66d9bd7450bb56697b04480
noahwekesa <noahwekesa0@gmail.com>
2024-02-21 13:40:13 +0300

    project initialiazation

---
+++ articles/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Article
+
+
+def index(request):
+    articles = Article.objects.all()
+    context = {'articles': articles}
+    return render(request, 'index.html', context)
